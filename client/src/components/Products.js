fea983e08ad4ac507f11af95eba62f02ce4ccc16
qsmit <qsmity@gmail.com>
2020-11-15 18:22:10 -0500

    fixed image sizing issue on product details page

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import products, { getAllProducts } from '../reducers/products'
+import { getAllProducts } from '../reducers/products'
 import { useHistory } from 'react-router-dom'
 
 const Products = (props) => {
